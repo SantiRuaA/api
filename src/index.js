51efba98bef819a69a914a0476cef7fe84d32247
Milton Ramirez <maramirez8665@misena.edu.co>
2023-05-13 19:22:24 -0500

    Agregando entrega

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const permisos = require("./routes/permiso");
 const rolPermisos = require("./routes/rolPermiso");
 const clientes = require("./routes/cliente");
 const paquetes = require("./routes/paquete");
+const entregas = require("./routes/entrega");
 const db = require("./db/database");
 const app = express();
 const port = process.env.PORT || 3030;
@@ -57,6 +58,8 @@ app.use('/rolPermiso', rolPermisos);
 app.use('/cliente',clientes);
 
 app.use('/paquete',paquetes);
+
+app.use('/entrega',entregas);
 
 app.listen(port,() => {
     console.log("Servidor trotando en el puerto: ", port);
---
+++ src/routes/entrega.js
@@ -0,0 +1,109 @@
+const Entrega = require('../models/entrega');
+const Lista = require('../models/listaPaquete');
+
+const router = require('express').Router()
+
+//obtener todos los usuarios
+router.get('/', async (req,res)=>{
+  const entregas = await Entrega.findAll();
+
+  res.json(entregas);
+});
+
+//Obtener un solo usuario
+router.get('/:id',async(req,res)=>{
+  const { id } = req.params;
+  const entrega = await Entrega.findByPk(id)
+  if(!entrega){
+    return res.status(404).json({
+      error:"No existe la entrega"
+    });
+  }
+  res.json(entrega);
+});
+
+//Crear usuario
+router.post('/', async (req,res)=>{
+  const { firmaDestinatario,fechaEntrega,idLista } = req.body;
+  //const ent = await Entrega.findOne({ where: {"Lo que no se vaya a repetir"}})
+  
+  if(!firmaDestinatario || !fechaEntrega || !idLista){
+    return res.status(400).json({
+        error:"Uno o mas campos vacios"
+    });
+  }
+  
+  /*if (ent){
+    return res.status(400).json({
+      error:"La entrega ya existe"
+    });
+  }*/
+
+  const listId = await Lista.findByPk(idLista);
+  if (!listId) {
+    return res.status(400).json({
+    error: 'La lista de paquetes no existe'
+    }); 
+  }
+
+
+  const entrega = await Entrega.create({firmaDestinatario,fechaEntrega,idLista})
+
+  res.json(entrega);
+});
+
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const entId = await Entrega.findByPk(id);
+  const { firmaDestinatario,fechaEntrega,idLista } = req.body;
+  /* const ent = await Entrega.findOne({ where: {"Lo que no se vaya a repetir"}}) */
+  
+  if(!firmaDestinatario || !fechaEntrega || !idLista){
+    return res.status(400).json({
+        error:"Uno o mas campos vacios"
+    });
+  }
+  
+  if (!entId) {
+    return res.status(404).json({
+      error:"No existe la entrega"
+    });
+  }
+
+ /*  if (ent){
+    return res.status(400).json({
+      error:"La entrega ya existe"
+    });
+  } */
+
+  const listId = await Lista.findByPk(idLista);
+  if (!listId) {
+    return res.status(400).json({
+    error: 'La lista de paquetes no existe'
+    }); 
+  }
+
+  await entId.update({firmaDestinatario,fechaEntrega,idLista})
+
+  res.json(entId);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const entId = await Entrega.findByPk(id);
+
+  if (!entId) {
+    return res.json({ msj: 'La entrega no existe o ya ha sido eliminado' });
+  }
+
+  await entId.destroy();
+
+  res.json({
+    msj: 'Paquete eliminado con exito',
+    entrega: entId
+  });
+});
+  
+
+module.exports = router

7d95e48f32f73c41ccbcd9465fa4c24f0629c874
srua <srua029@misena.edu.co>
2023-05-13 19:28:06 -0500

    Esto es necesario para agregando entrega

---
+++ src/models/novedad.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db/database");
+const TipoNovedad = require("./tipoNovedad");
+const Entrega = require("./entrega");
+
+const Novedad = db.define('novedad',{
+    idNovedad:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    descripcionNovedad:{
+        type: DataTypes.STRING,
+    },
+    idTipoNovedad:{
+        type:DataTypes.INTEGER,
+    },
+    idEntrega:{
+        type: DataTypes.INTEGER,
+    }
+});
+//Paquete.hasOne(TipoDocumentoUsuario, { foreignKey: 'idTipoDocumentoUsuario'});
+module.exports = Paquete;

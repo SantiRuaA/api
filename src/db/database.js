7ba73945306bfca3058928574b7b1008ff2b444c
srua <srua029@misena.edu.co>
2023-08-19 14:23:10 -0500

    host

--- src/db/database.js
+++ src/db/database.js
@@ -1,13 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('railway', 'root', '7vqEknHKJuz6ang7krPN', {
-  DB_HOST: 'containers-us-west-86.railway.app',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
-  /* one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-
   define: {
-    freezeTableName: true, // Evita la pluralización de nombres de tablas
-    timestamps: false, // Si no utilizas timestamps en tus modelos
+    freezeTableName: true,
+    timestamps: false,
   },
 });
 

c134b9f44d2844ca902d03a2a61c8d987176760f
srua <srua029@misena.edu.co>
2023-09-18 16:02:50 -0500

    goku

--- src/db/database.js
+++ src/db/database.js
@@ -1,13 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize('baurarptfwoixloaogzv', 'ukvlyeczouzao4bo', 'HUZ8nQbXeVVQTOHP31pK', {
-  DB_HOST: 'baurarptfwoixloaogzv-mysql.services.clever-cloud.com',
+  host: 'baurarptfwoixloaogzv-mysql.services.clever-cloud.com',
   dialect: 'mysql',
-  /* one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-
   define: {
-    freezeTableName: true, // Evita la pluralización de nombres de tablas
-    timestamps: false, // Si no utilizas timestamps en tus modelos
+    freezeTableName: true,
+    timestamps: false,
   },
 });
 

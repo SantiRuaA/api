612583d98be786ec8636129b756c1559f73fa3bd
Star Routing <soporte.starrouting@gmail.com>
2023-09-30 11:19:46 -0500

    na

--- src/db/database.js
+++ src/db/database.js
@@ -9,16 +9,15 @@ const db = new Sequelize('ber3cpmjqgibm884dabq', 'ukmtsqlo5wlfwezj', 'Inl9PNimgs
   },
 });
 
-module.exports = db;
-
-
-/*const db = new Sequelize('starrouting', 'root', '', {
+/* const db = new Sequelize('starrouting', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  /* one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  // one of | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle'
 
-/*define: {
-  freezeTableName: true, // Evita la pluralización de nombres de tablas
-  timestamps: false, // Evita la creación de los campos createdAt y updatedAt
-},
-});*/
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+}); */
+
+module.exports = db;
